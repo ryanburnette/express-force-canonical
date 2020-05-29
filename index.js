fc8fd2fdabeb28fd08a23cc0d918b213a0901cf7
Ryan Burnette <ryan.burnette@gmail.com>
2020-05-29 15:45:32 -0400

    v3.0.0

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 'use strict';
 
-module.exports = function(opts) {
+module.exports = function (opts) {
   if (!opts) {
     throw new Error('opts is required');
   }
-  if (!opts.chost) {
-    throw new Error('opts.chost is required');
+  if (!opts.host) {
+    throw new Error('opts.host is required');
   }
   opts.statusCode = opts.statusCode || 302;
   if (![300, 301, 302, 303, 307, 308].includes(opts.statusCode)) {
     throw new Error('opts.statusCode must be a redirect code');
   }
-  return function(req, res, next) {
-    if (opts.chost !== req.header('host')) {
-      var rproto = req.headers['x-forwarded-proto'] || req.protocol;
-      var redirectTo = (opts.proto || rproto) + '://' + opts.chost + req.path;
+  return function (req, res, next) {
+    var conditionsMet = false;
+    if (opts.host !== req.header('Host')) {
+      conditionsMet = true;
+    }
+    var reqProto = req.header('X-Forwarded-Proto') || req.protocol;
+    if (reqProto !== 'https') {
+      conditionsMet = true;
+    }
+    if (conditionsMet) {
       res.statusCode = opts.statusCode;
+      var redirectTo = 'https://' + opts.host + req.path;
       res.setHeader('Location', redirectTo);
       res.end();
       return;
--- readme.md
+++ readme.md
@@ -3,20 +3,15 @@
 [![repo](https://img.shields.io/badge/repository-Github-black.svg?style=flat-square)](https://github.com/ryanburnette/express-force-canonical)
 [![npm](https://img.shields.io/badge/package-NPM-green.svg?style=flat-square)](https://www.npmjs.com/package/@ryanburnette/express-force-canonical)
 
-Express or Node.js HTTP middleware to force a canonical host.
-
-Works behind a proxy or on Heroku.
+Express.js HTTP middleware to force HTTPS and a canonical host. Works behind a
+proxy or on Heroku. It enforces HTTPS as well. Oh, and it doesn't actually need
+Express.js, works with regular old Node.js HTTP as well.
 
 ```js
-var express = require('express');
-
-var app = express();
-
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(
     require('@ryanburnette/express-force-canonical')({
-      chost: 'www.mysite.com',
-      proto: 'https'
+      host: 'www.myawesomewebsite.com'
     })
   );
 }
