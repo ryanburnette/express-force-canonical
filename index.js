53366c806141b346a21efc638275a9ba41be0690
Ryan Burnette <ryan.burnette@gmail.com>
2020-05-29 15:55:42 -0400

    v4.0.0

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 'use strict';
 
-module.exports = function (opts) {
-  if (!opts) {
-    throw new Error('opts is required');
-  }
+module.exports = function (opts = {}) {
   if (!opts.host) {
     throw new Error('opts.host is required');
   }
-  opts.statusCode = opts.statusCode || 302;
+  opts.statusCode = opts.statusCode || 308;
   if (![300, 301, 302, 303, 307, 308].includes(opts.statusCode)) {
     throw new Error('opts.statusCode must be a redirect code');
   }
   return function (req, res, next) {
-    var conditionsMet = false;
-    if (opts.host !== req.header('Host')) {
-      conditionsMet = true;
-    }
-    var reqProto = req.header('X-Forwarded-Proto') || req.protocol;
-    if (reqProto !== 'https') {
-      conditionsMet = true;
-    }
-    if (conditionsMet) {
-      res.statusCode = opts.statusCode;
-      var redirectTo = 'https://' + opts.host + req.path;
-      res.setHeader('Location', redirectTo);
-      res.end();
-      return;
+    if (
+      (req.header('X-Forwarded-Proto') || req.protocol) !== 'https' ||
+      req.header('Host') !== opts.host
+    ) {
+      return res.redirect('https://' + opts.host + req.path, opts.statusCode);
     }
     next();
   };
--- readme.md
+++ readme.md
@@ -3,16 +3,12 @@
 [![repo](https://img.shields.io/badge/repository-Github-black.svg?style=flat-square)](https://github.com/ryanburnette/express-force-canonical)
 [![npm](https://img.shields.io/badge/package-NPM-green.svg?style=flat-square)](https://www.npmjs.com/package/@ryanburnette/express-force-canonical)
 
-Express.js HTTP middleware to force HTTPS and a canonical host. Works behind a
-proxy or on Heroku. It enforces HTTPS as well. Oh, and it doesn't actually need
-Express.js, works with regular old Node.js HTTP as well.
+Express.js middleware to force HTTPS and a canonical host. It works behind a
+proxy, and on Heroku.
 
 ```js
 if (process.env.NODE_ENV === 'production') {
-  app.use(
-    require('@ryanburnette/express-force-canonical')({
-      host: 'www.myawesomewebsite.com'
-    })
-  );
+  var host = 'www.myawesomewebiste.com';
+  app.use(require('@ryanburnette/express-force-canonical')({ host }));
 }
 ```
